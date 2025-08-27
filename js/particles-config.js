4cf658e0e00162e4585f6169f1ce6841f7723de7
krishnagupta-0109 <krishnaguptacoding@gmail.com>
2025-08-27 17:22:38 +0530

    Update particles-config.js

--- js/particles-config.js
+++ js/particles-config.js
@@ -1,39 +1,28 @@
-/* particles-config.js */
 particlesJS("particles-js", {
-  "particles": {
-    "number": { "value": 90, "density": { "enable": true, "value_area": 900 } },
-    "color": { "value": ["#00ffff", "#ff00ff", "#7fffd4"] },
-    "shape": { "type": "circle" },
-    "opacity": { "value": 0.6, "random": true },
-    "size": { "value": 3, "random": true },
-    "line_linked": {
-      "enable": true,
-      "distance": 140,
-      "color": "#00ffff",
-      "opacity": 0.25,
-      "width": 1
+  particles: {
+    number: { value: 60 },
+    color: { value: "#00ffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.3 },
+    size: { value: 2 },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#00ffff",
+      opacity: 0.4,
+      width: 1
     },
-    "move": {
-      "enable": true,
-      "speed": 1.8,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "bounce",
-      "attract": { "enable": true, "rotateX": 600, "rotateY": 1200 }
+    move: { enable: true, speed: 2 }
+  },
+  interactivity: {
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "push" }
+    },
+    modes: {
+      grab: { distance: 200, line_linked: { opacity: 0.8 } },
+      push: { particles_nb: 4 }
     }
   },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": { "enable": true, "mode": "grab" },
-      "onclick": { "enable": true, "mode": "push" },
-      "resize": true
-    },
-    "modes": {
-      "grab": { "distance": 180, "line_linked": { "opacity": 0.6 } },
-      "push": { "particles_nb": 5 }
-    }
-  },
-  "retina_detect": true
+  retina_detect: true
 });
